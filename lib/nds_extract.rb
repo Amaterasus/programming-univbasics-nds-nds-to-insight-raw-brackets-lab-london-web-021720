5c1e15acf0babecf483f3c298854366b66c6fec0
Skye Geraghty <skyetay03@googlemail.com>
2020-02-06 05:13:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,13 +25,14 @@ def directors_totals(nds)
   while index < nds.length do
     itterable = 0
     director_total = 0
+    director_name = nds[index][:name]
     
     while itterable < nds[index][:movies].length do
       director_total += nds[index][:movies][itterable][:worldwide_gross]
       
       itterable += 1
     end
-    totals nds[index][:name] += director_total
+    totals[director_name] += director_total
     
     index += 1
   end
