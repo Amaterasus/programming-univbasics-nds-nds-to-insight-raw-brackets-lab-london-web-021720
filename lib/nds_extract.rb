7dfb53345989666fa41fd049d90818593298458b
Skye Geraghty <skyetay03@googlemail.com>
2020-02-06 05:14:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -32,7 +32,7 @@ def directors_totals(nds)
       
       itterable += 1
     end
-    totals[director_name] => director_total
+    totals[director_name] = director_total
     
     index += 1
   end
